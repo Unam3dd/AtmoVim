81b2d3db0cec5bdd1578def14bf3662fab8f99ca
sam0verfl0w <samtalespro@gmail.com>
2023-12-22 11:14:15 +0100

    update AtmoVim

--- AtmoVim/lua/plugins/treesiter.lua
+++ AtmoVim/lua/plugins/treesiter.lua
@@ -1,14 +1,3 @@
-vim.keymap.set('n', 'T', function ()
-	vim.cmd([[TSInstall c]])
-	vim.cmd([[TSInstall cpp]])
-	vim.cmd([[TSInstall python]])
-	vim.cmd([[TSInstall bash]])
-	vim.cmd([[TSInstall json]])
-	vim.cmd([[TSInstall javascript]])
-	vim.cmd([[TSInstall rust]])
-	vim.cmd([[TSInstall go]])
-end, {desc = 'Install Treesiter parser'})
-
 require 'nvim-treesitter.configs'.setup {
 	ensure_installed = { "c", "cpp", "python", "json", "bash", "rust", "go", "javascript"},
 	sync_install = false,
