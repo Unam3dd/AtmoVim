4ff3d89ecfb9cb4eec2c7f5222fff04bdf810c13
Alex Robert <arobert@groupe-beaumanoir.fr>
2025-11-28 16:26:21 +0100

    ✨ Reintegrating a dashboard but using the startup.nvim plugin instead of alpha.nvim

---
+++ lua/plugins/statup.lua
@@ -0,0 +1,7 @@
+return {
+  "startup-nvim/startup.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
+  config = function()
+	  require "startup".setup(require"config.dashboard")
+  end
+}
