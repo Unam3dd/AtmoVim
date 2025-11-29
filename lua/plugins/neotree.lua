61c2dcf0d3588b1277ac134ed61612cc2d0c5b2f
Unam3dd <samtalespro@gmail.com>
2025-11-29 01:11:32 +0100

    ✅ Add some options to neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -10,10 +10,17 @@ return {
     lazy = false,
 	keys = {
 		{"<leader>e", ":Neotree toggle<CR>", desc = "Toggle Neotree", silent = true, noremap = true },
+    {"<C-n>", ":Neotree reveal<CR>", desc = "Reveal Neotree", silent = true, noremap = true },
 	},
 	config = function()
         require("neo-tree").setup({
+          popup_border_style = "rounded",
+          enable_diagnostics = true,
           filesystem = {
+            filtered_items = {
+              hide_dotfiles = false,
+              hide_gitignored = false,
+            },
             window = {
               mappings = {
                 ["<TAB>"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
