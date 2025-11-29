454f6fe890df7196058e59d7e2d8e7f831129e28
Unam3dd <samtalespro@gmail.com>
2025-11-29 19:47:21 +0100

    🧑‍💻 Changement du dashboard

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,32 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = 'v0.1.9',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	keys = {
-		{ '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
-    	{ '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
-		{ '<leader>fr', '<cmd>Telescope oldfiles<cr>', desc = 'Recent Files' },
-	}
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+
+  config = function()
+    local telescope = require('telescope')
+
+    telescope.setup({
+      defaults = {
+        layout_config = {
+          horizontal = { preview_width = 0.55 },
+          vertical = { mirror = false },
+        },
+        sorting_strategy = "ascending",
+        file_ignore_patterns = { "node_modules", ".git/" },
+      },
+      pickers = {
+        find_files = {
+          hidden = true,     -- parce que des fichiers cachés existent
+        },
+      },
+    })
+
+    -- Keymaps gentils
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files)
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep)
+    vim.keymap.set('n', '<leader>fb', builtin.buffers)
+    vim.keymap.set('n', '<leader>fr', builtin.oldfiles)
+  end
 }
