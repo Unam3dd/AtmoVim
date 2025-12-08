418f7f09dd412c305b4127f2e040203afd16d69f
Alex Robert <arobert@groupe-beaumanoir.fr>
2025-12-08 15:26:37 +0100

    ✨ Conform formats files when saving them + correction of a configuration error for the LSP ts_ls

--- lua/lsp/servers/ts_ls.lua
+++ lua/lsp/servers/ts_ls.lua
@@ -1,54 +1,62 @@
 -- Configuration pour ts_ls (TypeScript Language Server)
 
 return {
-  cmd = {
-    "typescript-language-server",
-    "--stdio",
-  },
-  root_dir = function(fname)
-    local root = vim.fs.root(fname, {
-      "package.json",
-      "tsconfig.json",
-      "jsconfig.json",
-      ".git",
-    })
-    return root or vim.fn.getcwd()
-  end,
-  single_file_support = true,
-  settings = {
-    implicitProjectConfiguration = {
-      checkJs = true,
-      experimentalDecorators = true,
-    },
-    typescript = {
-      suggest = {
-        completeFunctionCalls = true,
-      },
-      format = {
-        indentSize = 2,
-        tabSize = 2,
-        convertTabsToSpaces = true,
-      },
-      preferences = {
-        includePackageJsonAutoImports = "auto",
-        includeCompletionsForModuleExports = true,
-        includeCompletionsWithInsertText = true,
-      },
-    },
-    javascript = {
-      suggest = {
-        completeFunctionCalls = true,
-      },
-      format = {
-        indentSize = 2,
-        tabSize = 2,
-        convertTabsToSpaces = true,
-      },
-      preferences = {
-        includePackageJsonAutoImports = "auto",
-        includeCompletionsForModuleExports = true,
-        includeCompletionsWithInsertText = true,
-      },
-    },
-  },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	cmd = {
+		"typescript-language-server",
+		"--stdio",
+	},
+	root_dir = function(fname)
+		local root = vim.fs.root(fname, {
+			"package.json",
+			"tsconfig.json",
+			"jsconfig.json",
+			".git",
+		})
+		return root or vim.fn.getcwd()
+	end,
+	single_file_support = true,
+	settings = {
+		implicitProjectConfiguration = {
+			checkJs = true,
+			experimentalDecorators = true,
+		},
+		typescript = {
+			suggest = {
+				completeFunctionCalls = true,
+			},
+			format = {
+				indentSize = 2,
+				tabSize = 2,
+				convertTabsToSpaces = true,
+			},
+			preferences = {
+				includePackageJsonAutoImports = "auto",
+				includeCompletionsForModuleExports = true,
+				includeCompletionsWithInsertText = true,
+			},
+		},
+		javascript = {
+			suggest = {
+				completeFunctionCalls = true,
+			},
+			format = {
+				indentSize = 2,
+				tabSize = 2,
+				convertTabsToSpaces = true,
+			},
+			preferences = {
+				includePackageJsonAutoImports = "auto",
+				includeCompletionsForModuleExports = true,
+				includeCompletionsWithInsertText = true,
+			},
+		},
+	},
 }
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,49 +1,48 @@
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  keys = {
-    {
-      -- Customize or remove this keymap to your liking
-      "<leader>ft",
-      function()
-        require("conform").format({ async = true })
-      end,
-      mode = "",
-      desc = "Format buffer",
-    },
-  },
-  opts = {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>ft",
+			function()
+				require("conform").format({ async = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
 		formatters_by_ft = {
 			lua = { "stylua" },
 			python = { "isort", "black" },
-    		javascript = { "prettier" },
-    		typescript = { "prettier" },
-    		javascriptreact = { "prettier" },
-    		typescriptreact = { "prettier" },
-    		json = { "prettier" },
-    		yaml = { "prettier" },
-    		markdown = { "prettier" },
-    		html = { "prettier" },
-    		css = { "prettier" },
-    		scss = { "prettier" },
-    		c = { "clang_format" },
-    		cpp = { "clang_format" },
-    		go = { "gofmt", "goimports" },
-    		rust = { "rustfmt" },
-    		kotlin = { "ktlint" },
-    		sh = { "shfmt" },
-    		sql = { "sqlformat" },
-    		xml = { "xmllint" },
+			javascript = { "prettier" },
+			typescript = { "prettier" },
+			javascriptreact = { "prettier" },
+			typescriptreact = { "prettier" },
+			json = { "prettier" },
+			yaml = { "prettier" },
+			markdown = { "prettier" },
+			html = { "prettier" },
+			css = { "prettier" },
+			scss = { "prettier" },
+			c = { "clang_format" },
+			cpp = { "clang_format" },
+			go = { "gofmt", "goimports" },
+			rust = { "rustfmt" },
+			kotlin = { "ktlint" },
+			sh = { "shfmt" },
+			sql = { "sqlformat" },
+			xml = { "xmllint" },
 		},
 		notify_on_error = false,
 		default_format_opts = {
 			lsp_format = "fallback",
 		},
 
-		-- Set up format-on-save 
-		--format_on_save = { timeout_ms = 500 },
-		format_on_save = false,
+		-- Set up format-on-save
+		format_on_save = { timeout_ms = 500 },
 		formatters = {
 			shfmt = {
 				append_args = { "-i", "2" },
