227068a3f701ad0d67e5776e336a1208e710d256
Alex Robert <arobert@groupe-beaumanoir.fr>
2026-04-30 11:01:41 +0200

    ✨ feat(lsp): ajouter le serveur ESLint

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,6 +6,7 @@ local servers = {
 	"biome",
 	"cssls",
 	"dockerls",
+	"eslint",
 	"html",
 	"jsonls",
 	"marksman",
@@ -69,15 +70,15 @@ return {
 					local bufnr = args.buf
 					local bufopts = { noremap = true, silent = true, buffer = bufnr }
 					local maps = {
-						{ "n", "gd", vim.lsp.buf.definition },
-						{ "n", "gr", vim.lsp.buf.references },
-						{ "n", "gi", vim.lsp.buf.implementation },
-						{ "n", "gt", vim.lsp.buf.type_definition },
-						{ "n", "lK", vim.lsp.buf.hover },
+						{ "n", "gd",         vim.lsp.buf.definition },
+						{ "n", "gr",         vim.lsp.buf.references },
+						{ "n", "gi",         vim.lsp.buf.implementation },
+						{ "n", "gt",         vim.lsp.buf.type_definition },
+						{ "n", "lK",         vim.lsp.buf.hover },
 						{ "n", "<leader>rn", vim.lsp.buf.rename },
 						{ "n", "<leader>ca", vim.lsp.buf.code_action },
-						{ "n", "<leader>f", vim.lsp.buf.format },
-						{ "n", "<C-k>", vim.lsp.buf.signature_help },
+						{ "n", "<leader>f",  vim.lsp.buf.format },
+						{ "n", "<C-k>",      vim.lsp.buf.signature_help },
 					}
 					for _, map in ipairs(maps) do
 						vim.keymap.set(map[1], map[2], map[3], bufopts)
